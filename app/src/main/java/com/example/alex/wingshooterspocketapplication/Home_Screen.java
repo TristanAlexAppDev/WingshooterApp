11ea327e38c2f4f17f5fbb0a08c3cdb850ebd15e
CaptTAwesome <42463821+CaptTAwesome@users.noreply.github.com>
2018-08-29 14:25:50 +0200

    created home screen
    started switch for buttons as well

--- app/src/main/java/com/example/alex/wingshooterspocketapplication/Home_Screen.java
+++ app/src/main/java/com/example/alex/wingshooterspocketapplication/Home_Screen.java
@@ -2,12 +2,46 @@ package com.example.alex.wingshooterspocketapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
 
-public class Home_Screen extends AppCompatActivity {
+public class Home_Screen extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home__screen);
+
+        Button btnNAct = findViewById(R.id.btnNewActivity);
+        btnNAct.setOnClickListener(this);
+
+        Button btnHSeasons = findViewById(R.id.btnHuntingSeasons);
+        btnHSeasons.setOnClickListener(this);
+
+        Button btnGBID = findViewById(R.id.btnGamebird);
+        btnGBID.setOnClickListener(this);
+
+        Button btnMyHunts = findViewById(R.id.btnMyHunts);
+        btnMyHunts.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.btnNewActivity:
+                new NewHunt();
+                break;
+
+            case R.id.btnHuntingSeasons:
+               new  hsprovinceselection();
+                break;
+
+        }
+
     }
 }
