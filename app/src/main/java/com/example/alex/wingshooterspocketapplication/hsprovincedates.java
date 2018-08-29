7106e5d45f2d2be35156fcd110d212cef4051da9
CaptTAwesome <42463821+CaptTAwesome@users.noreply.github.com>
2018-08-29 13:13:50 +0200

    c

---
+++ app/src/main/java/com/example/alex/wingshooterspocketapplication/hsprovincedates.java
@@ -0,0 +1,14 @@
+package com.example.alex.wingshooterspocketapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class hsprovincedates extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hsprovincedates);
+    }
+}
