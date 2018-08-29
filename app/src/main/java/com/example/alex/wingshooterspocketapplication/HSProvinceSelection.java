493019b44735a6fce7bbd39a94b6045509f251fb
Alex <alex.mcmichael16@gmail.com>
2018-08-29 12:59:50 +0200

    Another Gui
    Got rid of pictures

--- app/src/main/java/com/example/alex/wingshooterspocketapplication/HSProvinceSelection.java
+++ app/src/main/java/com/example/alex/wingshooterspocketapplication/HSProvinceSelection.java
@@ -3,13 +3,13 @@ package com.example.alex.wingshooterspocketapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class HSProvinceSelection extends AppCompatActivity
+public class hsprovinceselection extends AppCompatActivity
 {
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.HSProvinceSelection);
+        setContentView(R.layout.hsprovinceselection);
     }
 }
