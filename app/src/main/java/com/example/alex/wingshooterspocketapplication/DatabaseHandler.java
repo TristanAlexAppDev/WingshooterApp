97c4b47a23e545d4e52859627a9c640593a21825
Alex <alex.mcmichael16@gmail.com>
2018-09-29 17:26:48 +0200

    Fixed the pictures
    Moved the pictures into the correct places. Also added a database, but it is not working need to fix it.

---
+++ app/src/main/java/com/example/alex/wingshooterspocketapplication/DatabaseHandler.java
@@ -0,0 +1,38 @@
+package com.example.alex.wingshooterspocketapplication;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHandler extends SQLiteOpenHelper
+{
+
+    //Database Version
+    private static final int DATABASE_VERSION = 1;
+
+    //Database name
+    private static final String DATABASE_NAME = "WingShootersDatabase";
+
+    //Table name 1
+    private static final String TABLE_NAME1 = "UserInfo";
+
+    //Table name 2
+    private static final String TABLE_NAME2 = "BirdInfo";
+
+    public DatabaseHandler(Context context,)
+    {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db)
+    {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+
+    }
+}
