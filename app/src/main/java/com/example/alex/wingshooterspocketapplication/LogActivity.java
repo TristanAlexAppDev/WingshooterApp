2e592d7b0efcddf1b4f5c93e1115c41e973e3216
CaptTAwesome <42463821+CaptTAwesome@users.noreply.github.com>
2018-10-08 17:49:28 +0200

    Change to Activity Log
    Changed layout to improve use and useability

--- app/src/main/java/com/example/alex/wingshooterspocketapplication/LogActivity.java
+++ app/src/main/java/com/example/alex/wingshooterspocketapplication/LogActivity.java
@@ -2,6 +2,8 @@ package com.example.alex.wingshooterspocketapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class LogActivity extends AppCompatActivity {
 
@@ -9,7 +11,25 @@ public class LogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
-
+        /* Setting up the drop down menu for the bird age*/
         setTitle("LOG ACTIVITY");
+
+        Spinner AgeSpin = (Spinner) findViewById(R.id.spnAge);
+
+        ArrayAdapter<String> adaptBAge = new ArrayAdapter<String>(LogActivity.this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.Bird_Age));
+
+        adaptBAge.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        AgeSpin.setAdapter(adaptBAge);
+        /* drop down menu for seen or shot */
+
+        Spinner SorS = (Spinner) findViewById(R.id.spnSorS);
+
+        ArrayAdapter<String> adaptSorS = new ArrayAdapter<String>(LogActivity.this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.SeenShot));
+
+        adaptSorS.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        SorS.setAdapter(adaptSorS);
+
     }
 }
