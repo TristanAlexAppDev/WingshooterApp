b80e11153f9c79185d8f08bad3139b436a60a3c7
iitzEnigma <iitzEnigma@users.noreply.github.com>
2018-10-10 14:04:45 +0200

    fixed gamebirdid
    made it lowercase

--- app/src/main/java/com/example/alex/wingshooterspocketapplication/gamebirdid.java
+++ app/src/main/java/com/example/alex/wingshooterspocketapplication/gamebirdid.java
@@ -10,6 +10,6 @@ public class gamebirdid extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.gameBirdIDMain);
+        setContentView(R.layout.gamebirdidmain);
     }
 }
