8977c59b60ef25449785a6fe8dc953697098229f
Alex <alex.mcmichael16@gmail.com>
2018-10-12 14:12:33 +0200

    have to again
    apparently again

--- app/src/main/java/com/example/alex/wingshooterspocketapplication/LogRegMainActivity.java
+++ app/src/main/java/com/example/alex/wingshooterspocketapplication/LogRegMainActivity.java
@@ -1,12 +1,18 @@
 package com.example.alex.wingshooterspocketapplication;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LogRegMainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public TextView TxtIDNUMlog;
+    public TextView edttxtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,6 +25,9 @@ public class LogRegMainActivity extends AppCompatActivity implements View.OnClic
 
         Button btnRegister = findViewById(R.id.btnReg);
         btnRegister.setOnClickListener(this);
+
+        TxtIDNUMlog = findViewById(R.id.TxtIDNUMlog);
+        edttxtPassword = findViewById(R.id.edttxtPassword);
     }
 
 
@@ -29,7 +38,7 @@ public class LogRegMainActivity extends AppCompatActivity implements View.OnClic
         switch (v.getId())
         {
             case R.id.btnLog:
-                HomeScreen();
+                LoginCheck();
                 break;
 
             case R.id.btnReg:
@@ -39,11 +48,29 @@ public class LogRegMainActivity extends AppCompatActivity implements View.OnClic
 
     }
 
-    public void HomeScreen()
+    public void LoginCheck()
     {
-        Intent intent = new Intent(this, Home_Screen.class);
-        startActivity(intent);
-        finish();
+        String userPass;
+        int userIDnum;
+        String dateExist = "";
+
+        userPass = edttxtPassword.getText().toString();
+        userIDnum = Integer.parseInt(TxtIDNUMlog.getText().toString());
+
+        if (userPass.equals("Admin123") && userIDnum == 199)
+        {
+            Toast.makeText(getApplicationContext(), "Welcome Admin", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(LogRegMainActivity.this, Home_Screen.class);
+            startActivity(intent);
+            finish();
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(), "Welcome user", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(LogRegMainActivity.this, Home_Screen.class);
+            startActivity(intent);
+            finish();
+        }
     }
 
     public void LoginRegister()
