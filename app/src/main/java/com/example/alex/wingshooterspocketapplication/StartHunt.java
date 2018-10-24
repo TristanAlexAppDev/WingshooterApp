23242e9c6f2d173a5da3bce6fda47e5e05ddf5ef
Alex <alex.mcmichael16@gmail.com>
2018-10-24 22:57:34 +0200

    small fix
    just to fix a minor issue

--- app/src/main/java/com/example/alex/wingshooterspocketapplication/StartHunt.java
+++ app/src/main/java/com/example/alex/wingshooterspocketapplication/StartHunt.java
@@ -32,8 +32,16 @@ public class StartHunt extends AppCompatActivity implements  View.OnClickListene
         TextView txtHeader = findViewById(R.id.txtActivityName);
         txtHeader.setText("Activity name: " + hunt);
         TextView txtLog1 = findViewById(R.id.txtReport1);
-        huntContent = huntContent + "/NEXT_LOG/" + LogActivity.NewLog;
-        txtLog1.setText(huntContent);
+        if (LogActivity.NewLog == null)
+        {
+            txtLog1.setText("");
+            huntContent = "";
+        }
+        else
+        {
+            huntContent = huntContent + "/NEXT_LOG/" + LogActivity.NewLog;
+            txtLog1.setText(huntContent);
+        }
     }
 
 
